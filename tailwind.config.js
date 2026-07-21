08e7b12a90da24b9d7c6e4b3bf118baeb6f43e9a
Rena <urujenirena@gmail.com>
2026-07-21 12:29:19 +0200

    feat:privacy page up until section6

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./*.html", "./dist/**/*.{html,js}"],
   theme: {
     extend: {
-    fontFamily: {
-      sans: ["Outfit", "sans-serif"],
-    }
+      fontFamily: {
+        sans: ["Outfit", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
+};
