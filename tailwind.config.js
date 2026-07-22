2dd6d9df39f5fb65c860faab687902ef1c82a39d
Guillain <ntwariguillain219@gmail.com>
2026-07-22 15:09:08 +0200

    updated tailwind.config  and added other color values

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,23 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-    fontFamily: {
-      sans: ["Outfit", "sans-serif"],
-    }
+      fontFamily: {
+        sans: ["Outfit", "sans-serif"],
+      },
+
+      colors: {
+        primary: "#00672E",
+        secondary: "#8DB600",
+        accent: "#F5A623",
+
+        text: {
+          primary: "#111827",
+          secondary: "#555555",
+        },
+
+        border: "#E5E7EB",
+      },
     },
   },
   plugins: [],
-}
+};
